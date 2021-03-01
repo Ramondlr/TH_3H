9afffdfd4242d6076078f8e4d00398899ae15de7
Ramon de la Rosa <rdl2@hi.is>
2021-03-01 20:36:22 +0000

    Bjó hér til möppu fyrir java klasana okkar

---
+++ Classes/Hotel.java
@@ -0,0 +1,18 @@
+/**
+ * Class: Software Development
+ * 
+ * Authors: Ísabella Montazeri, Jóhann Karl Hirst & Ramon de la Rosa
+ * 
+ * Teacher: Matthias Book
+ */
+// import stuff
+
+public abstract class Hotel{
+
+  public abstract void Method(){
+    /**
+     * getum breytt nafnið á klasanum seinna meir og breytt
+     */
+  }
+
+}
